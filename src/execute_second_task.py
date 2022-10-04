8ba0a7655a6cb68d6137b051cb5d91ed0b4318de
Riccardo Spolaor <riccardo.spolaor94@gmail.com>
2022-10-04 17:10:18 +0200

    Add docstring to `execute_second_task.py`

--- src/execute_second_task.py
+++ src/execute_second_task.py
@@ -1,17 +1,62 @@
 import argparse
 import json
+import numpy as np
 import os
 
 from utils import *
 
 
-def detect_russet(image: np.ndarray, h_means: List[np.ndarray],
-                  h_inv_covs: List[np.ndarray],
-                  roi_means: List[List[np.ndarray]],
-                  roi_inv_covs: List[List[np.ndarray]],
-                  roi_thresholds: List[List[int]],
-                  tweak_factor, image_name: str = None,
-                  verbose: bool = True) -> None:
+def detect_russet(image: np.ndarray, h_means: List[np.ndarray], h_inv_covs: List[np.ndarray],
+                  roi_means: List[List[np.ndarray]], roi_inv_covs: List[List[np.ndarray]],
+                  roi_thresholds: List[List[int]], tweak_factor, image_name: str = None,
+                  verbose: bool = True) -> Tuple[int, np.ndarray, np.ndarray]:
+    """
+    Function that executes the task of detecting the russet regions of a fruit.
+
+    It firstly detects the class of the fruit, then it looks for the russet regions that can be present according to the
+    class of the fruit.
+
+    If the task is run in `verbose` mode, then the procedure of the detection of the fruit class is plotted along with
+    the visualization of the russet regions in the fruit.
+
+    Parameters
+    ----------
+    image: np.ndarray
+        Colour image of the fruit whose russet has to be detected
+    h_means: List[np.ndarray]
+        List of the mean LAB colour values of the healthy part of the fruits (one mean per fruit class)
+    h_inv_covs: List[np.ndarray]
+        List of inverse covariance matrices of the healthy fruit parts computed on the LAB colour space (one covariance
+        matrix per fruit class)
+    roi_means: List[List[np.ndarray]]
+        List of list of mean LAB colour values of the russet regions of the fruits (one or multiple per fruit class)
+    roi_inv_covs: List[List[np.ndarray]]
+        List of list of inverse covariance matrices of the russet regions of the fruits computed on the LAB colour space
+        (one or multiple per fruit class)
+    roi_thresholds: List[List[int]]
+        List of list of thresholds. Pixels of the colour image having a Mahalanobis distance greater than a certain
+        thresholds are not considered part of the corresponding russet region (one or multiple per fruit class)
+    tweak_factor: float
+        Tweak factor to apply to the "Tweaked Otsu's Algorithm" in order to obtain the binary mask.
+    image_name: str, optional
+        Optional name of the image to visualize during the plotting operations
+    verbose: bool, optional
+        Whether to run the function in verbose mode or not (default: True)
+
+    Returns
+    -------
+    retval: int
+        Number of russet regions found in the fruit
+    stats: np.ndarray
+        Array of statistics about each russet region:
+            - The leftmost (x) coordinate which is the inclusive start of the bounding box in the horizontal direction;
+            - The topmost (y) coordinate which is the inclusive start of the bounding box in the vertical direction;
+            - The horizontal size of the bounding box;
+            - The vertical size of the bounding box;
+            - The total area (in pixels) of the russet.
+    centroids: np.ndarray
+        Array of centroid points about each russet region.
+    """
     # Apply median filter to the image
     f_img = cv.medianBlur(image, 5)
 
@@ -19,8 +64,7 @@ def detect_russet(image: np.ndarray, h_means: List[np.ndarray],
     gray_img = cv.cvtColor(f_img, cv.COLOR_BGR2GRAY)
 
     # Get the mask of the fruit
-    mask = get_fruit_mask(gray_img, ThresholdingMethod.TWEAKED_OTSU,
-                          tweak_factor=tweak_factor)
+    mask = get_fruit_mask(gray_img, ThresholdingMethod.TWEAKED_OTSU, tweak_factor=tweak_factor)
 
     # Mask the filtered image
     m_image = cv.cvtColor(mask, cv.COLOR_GRAY2BGR) + f_img
@@ -30,8 +74,7 @@ def detect_russet(image: np.ndarray, h_means: List[np.ndarray],
     channels = (1, 2)
 
     # Get fruit class
-    fruit_class = get_fruit_class(m_lab_image, h_means, h_inv_covs, channels,
-                                  display_image=image if verbose else None)
+    fruit_class = get_fruit_class(m_lab_image, h_means, h_inv_covs, channels, display_image=image if verbose else None)
 
     if verbose:
         print(f'Class of fruit = {fruit_class}')
@@ -45,10 +88,8 @@ def detect_russet(image: np.ndarray, h_means: List[np.ndarray],
 
     # Get the mask of each possible russet of the fruit and apply a bitwise
     # OR between it and the previous ROI mask
-    for m, c, t in zip(roi_means[fruit_class], roi_inv_covs[fruit_class],
-                       roi_thresholds[fruit_class]):
-        roi_mask += get_mahalanobis_distance_img(m_lab_image, m, c,
-                                                 t, channels)
+    for m, c, t in zip(roi_means[fruit_class], roi_inv_covs[fruit_class], roi_thresholds[fruit_class]):
+        roi_mask += get_mahalanobis_distance_img(m_lab_image, m, c, t, channels)
 
     # Apply a bitwise AND between the eroded mask and the ROI mask
     roi_mask = roi_mask & eroded_mask
@@ -60,8 +101,7 @@ def detect_russet(image: np.ndarray, h_means: List[np.ndarray],
     roi_mask = cv.morphologyEx(roi_mask, cv.MORPH_CLOSE, element)
 
     # Perform a connected components labeling to detect defects
-    retval, labels, stats, centroids = cv.connectedComponentsWithStats(
-        roi_mask)
+    retval, labels, stats, centroids = cv.connectedComponentsWithStats(roi_mask)
 
     if verbose:
         print(f'Detected {retval - 1} russet areas.')
@@ -74,59 +114,43 @@ def detect_russet(image: np.ndarray, h_means: List[np.ndarray],
         for i in range(1, retval):
             s = stats[i]
             # Draw a red ellipse around the defect
-            cv.ellipse(circled_russets,
-                       center=tuple(int(c) for c in centroids[i]),
-                       axes=(s[cv.CC_STAT_WIDTH] // 2 + 10,
-                             s[cv.CC_STAT_HEIGHT] // 2 + 10),
-                       angle=0, startAngle=0, endAngle=360,
-                       color=(0, 0, 255), thickness=3)
+            cv.ellipse(circled_russets, center=tuple(int(c) for c in centroids[i]),
+                       axes=(s[cv.CC_STAT_WIDTH] // 2 + 10, s[cv.CC_STAT_HEIGHT] // 2 + 10),
+                       angle=0, startAngle=0, endAngle=360, color=(0, 0, 255), thickness=3)
 
         if image_name is None:
             image_name = ''
 
-        plot_image_grid([highlighted_roi, circled_russets],
-                        ['Detected russets ROI', 'Detected russets areas'],
+        plot_image_grid([highlighted_roi, circled_russets], ['Detected russets ROI', 'Detected russets areas'],
                         f'Russets of the fruit in image {image_name}')
+    return retval - 1, stats[1:], centroids[1:]
 
 
-if __name__ == '__main__':
-    # Image names
-    nir_names, colour_names = [[f'C{j}_00000{i}.png' for i in range(1, 4)] for
-                               j in [0, 1]]
+def _main():
+    parser = argparse.ArgumentParser(description='Script for applying russet detection on a fruit.')
 
-    parser = argparse.ArgumentParser(
-        description='Script for applying russet detection on a fruit.')
+    parser.add_argument('fruit-image-path', metavar='Fruit image path', type=str,
+                        help='The path of the colour image of the fruit.')
 
-    parser.add_argument('fruit-image-path', metavar='Fruit image path',
-                        type=str, help='The path of the colour image of the '
-                                       'fruit.')
+    parser.add_argument('image-name', metavar='Image name', type=str, help='The name of the image.', default=None,
+                        nargs='?')
 
-    parser.add_argument('image-name', metavar='Image name',
-                        type=str, help='The name of the image.',
-                        default=None, nargs='?')
-
-    parser.add_argument('--config-file-path', '-cf', type=str,
-                        help='The path of the configuration file.',
-                        default=os.path.join(os.path.dirname(__file__),
-                                             f'config/config.json'),
-                        nargs='?', required=False)
-
-    parser.add_argument('--data-folder-path', '-d', type=str,
-                        help='The path of the data folder.',
-                        default=os.path.join(os.path.dirname(__file__),
-                                             f'data'),
-                        nargs='?', required=False)
-
-    parser.add_argument('--tweak-factor', '-tf', type=float, default=.4,
-                        nargs='?', help="Tweak factor for obtaining the "
-                                        "binary mask.",
+    parser.add_argument('--config-file-path', '-cf', type=str, help='The path of the configuration file.',
+                        default=os.path.join(os.path.dirname(__file__), f'config/config.json'), nargs='?',
                         required=False)
 
-    parser.add_argument('--no-verbose', '-nv', action='store_true',
-                        help='Skip the visualization of the results.')
+    parser.add_argument('--data-folder-path', '-d', type=str, help='The path of the data folder.',
+                        default=os.path.join(os.path.dirname(__file__), f'data'), nargs='?', required=False)
 
+    parser.add_argument('--tweak-factor', '-tf', type=float, default=.4, nargs='?',
+                        help='Tweak factor for obtaining the binary mask.', required=False)
+
+    parser.add_argument('--no-verbose', '-nv', action='store_true', help='Skip the visualization of the results.')
+
+    # Initialize parser
     arguments = parser.parse_args()
 
+    # Read colour image
     fruit_image_path = vars(arguments)['fruit-image-path']
     colour_image = cv.imread(fruit_image_path)
 
@@ -137,9 +161,11 @@ if __name__ == '__main__':
     tweak_factor = arguments.tweak_factor
     verbose = not arguments.no_verbose
 
+    # Get config file
     with open(config_file_path, 'r') as j:
         config_dictionary = json.load(j)
 
+    # Get means, inverse covariance matrices and other information from the config file
     healthy_fruit_means = config_dictionary['healthy_fruit_means']
     healthy_fruit_inv_covs = config_dictionary['healthy_fruit_inv_covs']
     roi_means = config_dictionary['roi_means']
@@ -147,25 +173,25 @@ if __name__ == '__main__':
     roi_thresholds = config_dictionary['roi_thresholds']
     roi_related_fruit = config_dictionary['roi_related_fruit']
 
-    healthy_fruit_means = [np.load(os.path.join(data_folder_path, n))
-                           for n in healthy_fruit_means]
-    healthy_fruit_inv_covs = [np.load(os.path.join(data_folder_path, n))
-                              for n in healthy_fruit_inv_covs]
-    roi_means = [np.load(os.path.join(data_folder_path, n))
-                 for n in roi_means]
-    roi_inv_covs = [np.load(os.path.join(data_folder_path, n))
-                    for n in roi_inv_covs]
+    healthy_fruit_means = [np.load(os.path.join(data_folder_path, n)) for n in healthy_fruit_means]
+    healthy_fruit_inv_covs = [np.load(os.path.join(data_folder_path, n)) for n in healthy_fruit_inv_covs]
+    roi_means = [np.load(os.path.join(data_folder_path, n)) for n in roi_means]
+    roi_inv_covs = [np.load(os.path.join(data_folder_path, n)) for n in roi_inv_covs]
 
+    # Assign roi to each fruit
     roi_means_sorted = [[] for _ in range(len(healthy_fruit_means))]
     roi_inv_cov_sorted = [[] for _ in range(len(healthy_fruit_means))]
     roi_thresholds_sorted = [[] for _ in range(len(healthy_fruit_means))]
 
-    for m, c, t, r in zip(roi_means, roi_inv_covs, roi_thresholds,
-                          roi_related_fruit):
+    for m, c, t, r in zip(roi_means, roi_inv_covs, roi_thresholds, roi_related_fruit):
         roi_means_sorted[r].append(m)
         roi_inv_cov_sorted[r].append(c)
         roi_thresholds_sorted[r].append(t)
 
-    detect_russet(colour_image, healthy_fruit_means, healthy_fruit_inv_covs,
-                  roi_means_sorted, roi_inv_cov_sorted, roi_thresholds_sorted,
-                  tweak_factor, image_name = None)
+    # Apply russet detection
+    detect_russet(colour_image, healthy_fruit_means, healthy_fruit_inv_covs, roi_means_sorted, roi_inv_cov_sorted,
+                  roi_thresholds_sorted, tweak_factor, image_name=image_name, verbose=verbose)
+
+
+if __name__ == '__main__':
+    _main()
