607ba3b9f11b116836aaa7b1b322336f626b06cb
Riccardo Spolaor <riccardo.spolaor94@gmail.com>
2022-10-30 10:55:15 +0100

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,4 +2,4 @@
 Utils module
 """
 from __future__ import absolute_import
-from . import colour as c, colour_threshold, edge, general, threshold
+from . import colour, colour_threshold, edge, general, threshold
